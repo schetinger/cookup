4bfa16c018522bf78d43bc031692a78f1faebf0a
schetinger <rafaelschetinger1@gmail.com>
2025-10-27 15:17:37 -0300

    grid certo

--- src/app/_components/sec1.tsx
+++ src/app/_components/sec1.tsx
@@ -7,39 +7,39 @@ export function Sec1 ({className}:{className:string}){
     return(
         <section className={className}>
             <p className="text-8xl p-4 text-center font-bold">Conheça as melhores receitas</p>
-            <div className="grid-cols-2 gap-2 grid ">
+            <div className="columns-2 gap-3">
                 <Image
                         src={pancake}
                         alt="Pancake"
-                        width={500}
-                        height={500}
-                        className="rounded-xl"
+                        width={400}
+                        height={400}
+                        className="rounded-xl columns-1 w-full mb-3 break-inside-avoid"
                     />
                 <Image
                         src={sandwich}
                         alt="Sandwich"
-                        width={200}
-                        height={200}
-                        className="rounded-xl"
+                        width={400}
+                        height={400}
+                        className="rounded-xl columns-2 w-full mb-3 break-inside-avoid"
                 />
                 <Image
                     src={ribs}
                     alt="Ribs"
                     width={400}
                     height={400}
-                    className="rounded-xl"
+                    className="rounded-xl h-full columns-1 w-full mb-3 break-inside-avoid"
                 />
                 <Image
                     src={cake}
                     alt="Cake"
-                    width={350}
-                    height={350}
-                    className="rounded-xl"
+                    width={400}
+                    height={400}
+                    className="rounded-xl columns-2 w-full mb-3 break-inside-avoid"
                                 />
 
 
                 </div> 
-                <p className="text-8xl text-center p-4 font-bold">E faça você mesmo em casa</p>
+                <p className="text-8xl text-center p-4 font-bold">E faça no conforto de sua casa</p>
             </section>
     )
     
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default async function Home() {
     <div className="h-screen flex flex-col">
       <Navbar className=" flex-1 top-0 left-0 w-full z-10"></Navbar>  
       <main className=" bg-gray-500 h-full overflow-y-scroll snap-y snap-mandatory scroll-smooth">
-        <Sec1 className="padding-top snap-start h-full bg-amber-200 grid grid-cols-3 items-start justify-between"></Sec1>
+        <Sec1 className="pt-10 padding-top snap-start h-full bg-amber-200 grid grid-cols-3 items-start justify-between"></Sec1>
         <section className=" snap-start h-full bg-blue-500">
           <p>paradas</p>
         </section>
